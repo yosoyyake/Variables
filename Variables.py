c9e5aa35474cc41ae4e984ee85dbab18915bf4a8
yosoyyake <73201456+yosoyyake@users.noreply.github.com>
2020-10-21 13:51:47 -0400

    Add files via upload
    La Mangulina

---
+++ Variables.py
@@ -0,0 +1,12 @@
+var_año=2020
+print("El año actual es:", var_año)
+var_pi=3.141592
+print("El valor de PI es:", var_pi)
+var_true="Verdadero"
+print(var_true)
+var_lista="Leon, Gato, Perro, Caballo, Vaca"
+print(var_lista)
+var_otralis="Melon, Sandia, Mango, Fresa, Cereza"
+print(var_otralis [29:35])
+
+
